6822c16c27dc03766408a0ca79d760f0cdc4c9aa
tleymarie <tleymarie@norauto.com>
2026-06-01 13:50:51 +0200

    try to fix default page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 import AdminView from '../views/AdminView.vue';
+import HomeView from '../views/HomeView.vue';
 import LoginView from '../views/LoginView.vue';
-import { useSchedulesStore } from "../stores/schedules.ts";
-import { storeToRefs } from "pinia";
 
-const defaultCompetition = storeToRefs(useSchedulesStore()).defaultCompetition;
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      redirect: `/${defaultCompetition.value as unknown as string}`,
+      redirect: "/2026_Cluny",
     },
     {
       path: '/:competition',
